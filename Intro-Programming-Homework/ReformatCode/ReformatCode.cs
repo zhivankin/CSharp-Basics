8afe4317374aab20fcb95f69c66ae106902d6324
Atanas Zhivankin <n_zhivankin@abv.bg>
2016-03-24 18:00:06 +0200

    Added files via upload

---
+++ Intro-Programming-Homework/ReformatCode/ReformatCode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class ReformatCode
+{
+    static void Main()
+    {
+        Console.WriteLine("Hi, I am already a perfectly formatted program.");
+        Console.WriteLine("Numbers and squares:");
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(i + " --> " + i * i); 
+        }
+    }
+}
