275ac24ccd1d6df91584a4a60c61c2a1da5a5749
Atanas Zhivankin <n_zhivankin@abv.bg>
2016-03-24 17:56:59 +0200

    Added files via upload

---
+++ Intro-Programming-Homework/AgeAfter10Years/AgeAfter10Years.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class AgeAfter10Years
+{ 
+    static void Main()
+    {  
+        Console.Write("Enter your birthday in format(dd.mm.yyyy): ");
+        DateTime userBirthday = DateTime.Parse(Console.ReadLine());
+        long result = DateTime.Today.Subtract(userBirthday).Ticks;
+        Console.WriteLine("You are {0} years old.", new DateTime(result).Year - 1);
+        Console.WriteLine("After 10 years you will be {0} years old.", new DateTime(result).AddYears(10).Year - 1);
+    }
+}
+
---
+++ Intro-Programming-Homework/PrintLongSequence/PrintLongSequence.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class PrintLongSequence
+{
+    static void Main()
+    {
+        for (int i = 2; i <= 1001; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.Write(i);
+            }
+            else
+            {
+                Console.Write(-i);
+            }
+            if (i < 1001)
+            {
+                Console.Write(",");
+            }
+        }
+    }
+}
+
